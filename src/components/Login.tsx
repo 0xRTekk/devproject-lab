1d5139cb6bfebe734a93890eae07421829112474
Rémi Sulpice <sulpice.remi@gmail.com>
2026-02-10 16:25:24 +0100

    ✨ Feat(Account) : Display user info JSON

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -2,13 +2,31 @@
 
 import LoginButton from "@/components/LoginButton";
 import LogoutButton from "@/components/LogoutButton";
+import useAuth from "@/hooks/useAuth";
 
 export default function Login() {
+    const { user, loading } = useAuth();
+
+    if (loading) return <div aria-busy="true">Loading…</div>;
+
     return (
         <>
-            <LoginButton />
-            <br />
-            <LogoutButton />
+            {user ? (
+                <div>
+                    <div>
+                        Connected as: {user.email ?? user.user_metadata?.name ?? user.id}
+                    </div>
+                    <pre style={{ whiteSpace: "pre-wrap", fontSize: 12 }}>
+                        {JSON.stringify(user, null, 2)}
+                    </pre>
+                    <LogoutButton />
+                </div>
+            ) : (
+                <div>
+                    <div>Not signed in</div>
+                    <LoginButton />
+                </div>
+            )}
         </>
     );
 }
